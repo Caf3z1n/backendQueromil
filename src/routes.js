01e3cd2de4dcc1df5141d4f7b972beab56172276
Pedro Henrique <pedrosg2003@gmail.com>
2020-10-28 15:46:17 -0300

    Cria rota de teste

--- src/routes.js
+++ src/routes.js
@@ -18,6 +18,8 @@ routes.post('/users', UserController.store);
 
 routes.post('/sessions', SessionController.store);
 
+routes.get('/', (req, res) => res.send('Ok'));
+
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
